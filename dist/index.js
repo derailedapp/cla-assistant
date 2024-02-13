1e0251befe15fa2212f404bfbf5e6db70b99675d
VincentRPS <vincentbusiness55@gmail.com>
2024-02-14 06:35:58 +0800

    Update index.js
    Signed-off-by: VincentRPS <vincentbusiness55@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -592,10 +592,10 @@ function getComment() {
             //TODO: check the below regex
             // using a `string` true or false purposely as github action input cannot have a boolean value
             if ((0, getInputs_1.getUseDcoFlag)() === 'true') {
-                return response.data.find(comment => comment.body.match(/\bTrain Conductor$/));
+                return response.data.find(comment => comment.author.login === "steamweaver");
             }
             else if ((0, getInputs_1.getUseDcoFlag)() === 'false') {
-                return response.data.find(comment => comment.body.match(/\bTrain Conductor$/));
+                return response.data.find(comment => comment.author.login === "steamweaver");
             }
         }
         catch (error) {
